f995c79acd0c6c49787b8a43cb809f5ae784faa2
cenda <mail@junajan.cz>
2016-01-02 12:22:20 +0100

    Added openings script

---
+++ openings.js
@@ -0,0 +1,19 @@
+require('colors');
+var async = require("async");
+var moment = require("moment");
+var server = require('./modules/Server');
+var conf = require("./config/public")(server.app);
+var app = server.run(conf);
+app.set("db",require('./modules/Mysql')(conf.mysql));
+app.set("config", conf);
+
+var Openings = require('./modules/Openings')(app);
+
+
+console.log('Loading openings');
+
+Openings.importData(function(err, res) {
+	if(err) throw err;
+	console.log('Openings was loaded: ', res);
+	process.exit(0);
+});
