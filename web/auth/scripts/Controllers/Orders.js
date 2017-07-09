839b5e9da637875062f98764a8326f261ffcc941
Jan Juna <mail@janjuna.cz>
2017-07-09 19:58:12 +0200

    Fix actual profit/loss counter on orders page

--- web/auth/scripts/Controllers/Orders.js
+++ web/auth/scripts/Controllers/Orders.js
@@ -1,6 +1,6 @@
 Sandy.controller ( "Orders", [
-    '$scope', '$timeout', 'Orders', '$location',
-    function ($scope, $timeout, Orders, $location) {
+    '$scope', '$timeout', 'Orders', '$location', '$rootScope',
+    function ($scope, $timeout, Orders, $location, $rootScope) {
 
 			var queryMethod = "query";
 			if('/orders-grouped' === $location.path())
@@ -10,13 +10,14 @@ Sandy.controller ( "Orders", [
 	    	Orders[queryMethod](function(res) {
 					$scope.orders = res;
 					$scope.load && $timeout($scope.load, 1000);
+          $rootScope.countActualProfitLoss(res)
 	    	});
     	};
 
     	$scope.load();
 
-        $scope.$on('$destroy', function() {
-            $scope.load = null;
-        });
+			$scope.$on('$destroy', function() {
+					$scope.load = null;
+			});
     }
 ]);
